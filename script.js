08245092c53a8ce58682d57d534c95eaf0c0a4f3
NinzaCoder-js <subhonshohabduraxmonov@gmail.com>
2024-05-08 08:25:49 +0500

    all bugs are fixed

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 let submit = document.querySelector("#btn2");
 let submit1 = document.querySelector("#btn1");
 let img = document.querySelector('img');
+let selected = document.querySelector('body');
 
 submit.addEventListener('click', ()=>{
-    let selected = document.querySelector('body');
     selected.style.background='black';
     selected.style.transition='All 0.4s';
     submit.style.background='red';
@@ -15,7 +15,6 @@ submit.addEventListener('click', ()=>{
 })
 
 submit1.addEventListener('click', ()=>{
-    let selected = document.querySelector('body');
     selected.style.background='white';
     selected.style.transition='All 0.4s';
     submit1.style.background='red';
